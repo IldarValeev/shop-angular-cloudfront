095849988b22569b2eefa7a587a3d3f389b003d2
ildar.valeev <ildar189@gmail.com>
2024-03-24 17:46:14 +0100

    feat: task-3 Updated urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://vlpazo74p8.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://gdzwng0qre.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
